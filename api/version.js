bf0c8027a17557fe0d66e1a62ed10c9e0393ac06
diaboyy <151085639+diaboyy@users.noreply.github.com>
2025-11-14 22:32:13 +0300

    Create version.js

---
+++ api/version.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+    return res.status(200).json({
+        latest: "1.0.0",
+        downloadUrl: "https://nolvra.com/downloads/LUXA_v1.pkg",
+        changelog: "Initial release"
+    });
+}
